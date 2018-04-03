c7ffc4273d677348fb268c4bb58b5e391c149232
Peter Krevenets <peter.k.nets@gmail.com>
2018-04-03 09:27:26 +0300

    Update betterSolution

--- string/isIPv4Address_cf_10/betterSolution.js
+++ string/isIPv4Address_cf_10/betterSolution.js
@@ -4,9 +4,3 @@ function isIPv4Address(s) {
     r.length === 4 && r.every(x => x != "" && !isNaN(x) && x >= 0 && x < 256)
   );
 }
-
-isIPv4Address = s =>
-  s.split`.`.every(
-    e => (++c, (e == "0" || +e) && e >= 0 && e < 256),
-    (c = 0)
-  ) && c == 4;

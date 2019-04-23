cbfd9d1e3f8995d79495c44fb8e7c73fe73ef43c
GuRuGu <peter.k.nets@gmail.com>
2019-04-23 09:58:19 +0300

    Solve rectangleRotation from CodeSignal

---
+++ integer/rectangleRotation_cf-101/otherSolutions.js
@@ -0,0 +1,7 @@
+function rectangleRotation(a, b) {
+  var pointsA = Math.floor(Math.sqrt(a * a / 2)),
+    pointsB = Math.floor(Math.sqrt(b * b / 2));
+  return (pointsA * pointsB + Math.floor((pointsA + pointsB) / 2)) * 2 + 1;
+}
+
+/////////////////////
--- integer/rectangleRotation_cf-101/solution.js
+++ integer/rectangleRotation_cf-101/solution.js
@@ -1,1 +1,20 @@
-function rectangleRotation(a, b) {}
+function rectangleRotation(a, b) {
+  const first = Math.floor(b / (2 * Math.sqrt(2)) * 2);
+  const second = Math.floor(a / (2 * Math.sqrt(2)) * 2);
+  const result = second * first + (second + 1) * (first + 1);
+  return result % 2 === 0 ? result - 1 : result;
+}
+
+const q1 = [6, 4]; // 23
+const q2 = [30, 2]; // 65
+const q3 = [8, 6]; // 49
+const q4 = [16, 20]; // 333
+const q5 = [20, 32]; // 653
+const q6 = [30, 26]; // 795
+
+console.log(rectangleRotation(...q1));
+console.log(rectangleRotation(...q2));
+console.log(rectangleRotation(...q3));
+console.log(rectangleRotation(...q4));
+console.log(rectangleRotation(...q5));
+console.log(rectangleRotation(...q6));

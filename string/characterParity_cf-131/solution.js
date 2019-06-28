02e1e5d03b4f91031b061f392f3c68a210641f7b
GuRuGu <peter.k.nets@gmail.com>
2019-06-28 10:09:03 +0300

    Set up characterParity

---
+++ string/characterParity_cf-131/solution.js
@@ -0,0 +1,38 @@
+/*
+digit or not a digit at all.
+
+Example
+
+For symbol = '5', the output should be
+characterParity(symbol) = "odd";
+For symbol = '8', the output should be
+characterParity(symbol) = "even";
+For symbol = 'q', the output should be
+characterParity(symbol) = "not a digit".
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] char symbol
+
+[output] string
+
+*/
+
+function characterParity(a) {}
+
+const q1 = "5"; // "odd"
+const q2 = "8"; // "even"
+const q3 = "q"; // "not a digit"
+const q4 = "1"; // "odd"
+const q5 = "2"; // "even"
+const q6 = "7"; // "odd"
+const q7 = "9"; // "odd"
+
+console.log(characterParity(q1));
+console.log(characterParity(q2));
+console.log(characterParity(q3));
+console.log(characterParity(q4));
+console.log(characterParity(q5));
+console.log(characterParity(q6));
+console.log(characterParity(q7));

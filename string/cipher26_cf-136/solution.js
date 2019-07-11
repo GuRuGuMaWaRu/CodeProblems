71255eda3a07d5f8462ef645672ec740a2e45270
GuRuGu <peter.k.nets@gmail.com>
2019-07-11 09:50:13 +0300

    Set up cipher26

---
+++ string/cipher26_cf-136/solution.js
@@ -0,0 +1,50 @@
+/*
+You've intercepted an encrypted message, and you are really curious about its contents. You were able to find out that the message initially contained only lowercase English letters, and was encrypted with the following cipher:
+
+Let all letters from 'a' to 'z' correspond to the numbers from 0 to 25, respectively.
+The number corresponding to the ith letter of the encrypted message is then equal to the sum of numbers corresponding to the first i letters of the initial unencrypted message modulo 26.
+Now that you know how the message was encrypted, implement the algorithm to decipher it.
+
+Example
+
+For message = "taiaiaertkixquxjnfxxdh", the output should be
+cipher26(message) = "thisisencryptedmessage".
+
+The initial message "thisisencryptedmessage" was encrypted as follows:
+
+letter 0: t -> 19 -> t;
+letter 1: th -> (19 + 7) % 26 -> 0 -> a;
+letter 2: thi -> (19 + 7 + 8) % 26 -> 8 -> i;
+etc.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string message
+
+An encrypted string containing only lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ message.length ≤ 200.
+
+[output] string
+
+A decrypted message.
+
+*/
+
+function cipher26(message) {}
+
+const q1 = "taiaiaertkixquxjnfxxdh"; // "thisisencryptedmessage"
+const q2 = "ibttlprimfymqlpgeftwu"; // "itsasecrettoeverybody"
+const q3 = "ftnexvoky"; // "fourtytwo"
+const q4 = "taevzhzmashvjw"; // "thereisnospoon"
+const q5 = "abdgkpvcktdoanbqgxpicxtqon"; // "abcdefghijklmnopqrstuvwxyz"
+const q6 = "z"; // "z"
+
+console.log(cipher26(q1));
+console.log(cipher26(q2));
+console.log(cipher26(q3));
+console.log(cipher26(q4));
+console.log(cipher26(q5));
+console.log(cipher26(q6));

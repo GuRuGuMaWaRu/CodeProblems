7391a87e84865aa5c5874cb3f17cb32cf8c6d033
GuRuGu <peter.k.nets@gmail.com>
2020-01-18 09:10:25 +0200

    Solve combs

--- string/combs_cf-144/solution.js
+++ string/combs_cf-144/solution.js
@@ -38,17 +38,73 @@ The minimum length of a purse Miss X needs.
 */
 
 function combs(comb1, comb2) {
-  return 5;
+  const replaced1 = comb1.replace(/\.|\*/g, match => {
+    switch (match) {
+      case '*':
+        return 1;
+      default:
+        return 0;
+    }
+  });
+  const replaced2 = comb2.replace(/\.|\*/g, match => {
+    switch (match) {
+      case '*':
+        return 1;
+      default:
+        return 0;
+    }
+  });
+
+  const comb1Padded = replaced1.padEnd(
+    replaced1.length + replaced2.length + replaced1.length,
+    '0'
+  );
+  const comb2Padded = replaced2.padEnd(
+    replaced1.length + replaced2.length,
+    '0'
+  );
+
+  const comb1Num = parseInt(comb1Padded, 2);
+  const comb2Num = parseInt(comb2Padded, 2);
+
+  let finalLength = comb1.length + comb2.length;
+  const testLength = comb1.length + comb2.length;
+  const biggestComb =
+    comb1.length >= comb2.length ? comb1.length : comb2.length;
+
+  for (let i = 1; i < testLength; i++) {
+    const shift = comb1Num >> i;
+    const result = shift & comb2Num;
+
+    if (result === 0) {
+      if (i < biggestComb) {
+        const length =
+          testLength - i >= biggestComb ? testLength - i : biggestComb;
+
+        if (length < finalLength) {
+          finalLength = length;
+        }
+      } else {
+        const length = i;
+
+        if (length < finalLength) {
+          finalLength = length;
+        }
+      }
+    }
+  }
+
+  return finalLength;
 }
 
-const q1 = ["*..*", "*.*"]; // 5
-const q2 = ["*...*", "*.*"]; // 5
-const q3 = ["*..*.*", "*.***"]; // 9
-const q4 = ["*.*", "*.*"]; // 4
-const q5 = ["*.**", "*.*"]; // 5
+const q1 = ['*..*', '*.*']; // 5
+const q2 = ['*...*', '*.*']; // 5
+const q3 = ['*..*.*', '*.***']; // 9
+const q4 = ['*.*', '*.*']; // 4
+const q5 = ['*.**', '*.*']; // 5
 
-console.log("q1:", combs(...q1));
-console.log("q2:", combs(...q2));
-console.log("q3:", combs(...q3));
-console.log("q4:", combs(...q4));
-console.log("q5:", combs(...q5));
+console.log('q1:', combs(...q1));
+console.log('q2:', combs(...q2));
+console.log('q3:', combs(...q3));
+console.log('q4:', combs(...q4));
+console.log('q5:', combs(...q5));

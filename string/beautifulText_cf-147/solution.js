c70aa05f8d7e49eeca66ac75bf34f08c9f75e8ea
GuRuGu <peter.k.nets@gmail.com>
2021-07-01 10:32:48 +0300

    Solve beautifulText

--- string/beautifulText_cf-147/solution.js
+++ string/beautifulText_cf-147/solution.js
@@ -4,32 +4,76 @@ Consider a string containing only letters and whitespaces. It is allowed to repl
 Given a string and some integers l and r (l ≤ r), check if it's possible to obtain a beautiful text from the string with a text width that's within the range [l, r].
 
 */
-function beautifulText(inputString, l, r) {
-  // let isBeautiful = false;
+// const recursivness = (masterLine, remainingWords) => {
+//   if (remainingWords.length === 0) {
+//     return true;
+//   }
 
-  // for (let i = l - 1; i < r; i++) {
-  //   if (inputString[i] === ' ') {
-  //     isBeautiful = true;
+//   const testLine = [];
 
-  //     for (let j = i * 2 + 1; j <= inputString.length; j += i + 1) {
-  //       console.log(
-  //         `length: ${inputString.length}; i: ${i}; j: ${j}; char: ${inputString[j]}`
-  //       );
-  //       if (inputString[j] !== ' ') {
-  //         // isBeautiful = false;
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
+//   for (let i = 0; i < remainingWords.length; i++) {
+//     testLine.push(remainingWords[i]);
+//     const joinedTestLine = testLine.join(' ');
 
-  // return isBeautiful;
+//     if (joinedTestLine.length === masterLine.length) {
+//       recursivness(masterLine, remainingWords.slice(i + 1));
+//     } else if (joinedTestLine.length > masterLine.length) {
+//       break;
+//     }
+//   }
 
-  // we use a loop with a step from l to r, and we ask ourselves at every junction whether we have a "space" before or after - if there is no space, then we
-  // increase our step value and start anew
+//   return false;
+// };
 
-  // after we find the right step value, we should use it to split our string into chunks, trim hanging spaces, check if all resulting substring are equal in length
-}
+const beautifulText = (inputString, l, r) => {
+  debugger;
+  let isBeautiful = false;
+  const masterLine = [];
+  const inputStringWords = inputString.split(' ');
+
+  for (let i = 0; i < inputStringWords.length; i++) {
+    masterLine.push(inputStringWords[i]);
+    const masterLineLength = masterLine.join(' ').length;
+
+    if (
+      masterLineLength >= l &&
+      masterLineLength <= r &&
+      masterLineLength <= inputString.length / 2
+    ) {
+      let testLine = [];
+
+      for (let j = i + 1; j < inputStringWords.length; j++) {
+        testLine.push(inputStringWords[j]);
+        const testLineLength = testLine.join(' ').length;
+
+        if (testLineLength > masterLineLength) {
+          break;
+        }
+
+        if (
+          testLineLength === masterLineLength &&
+          j === inputStringWords.length - 1
+        ) {
+          isBeautiful = true;
+          break;
+        }
+
+        if (testLineLength === masterLineLength) {
+          testLine = [];
+          continue;
+        }
+      }
+
+      // console.log(masterLine);
+    }
+
+    if (isBeautiful) {
+      break;
+    }
+  }
+
+  return isBeautiful;
+};
 
 const q1 = ['Look at this example of a correct text', 5, 15]; // true
 const q2 = ['abc def ghi', 4, 10]; // false
@@ -44,11 +88,13 @@ const q10 = ['skspv iakqh ygzwz ntkmi xqhpj', 3, 7]; // true
 const q11 = ['skspv iakqh ygzwz ntkmi xqhpj', 8, 9]; // false
 
 console.log('q1:', beautifulText(...q1));
-// console.log('q2:', beautifulText(...q2));
-// console.log('q3:', beautifulText(...q3));
-// console.log('q4:', beautifulText(...q4));
-// console.log('q5:', beautifulText(...q5));
-// console.log('q6:', beautifulText(...q6));
-// console.log('q7:', beautifulText(...q7));
-// console.log('q8:', beautifulText(...q8));
-// console.log('q9:', beautifulText(...q9));
+console.log('q2:', beautifulText(...q2));
+console.log('q3:', beautifulText(...q3));
+console.log('q4:', beautifulText(...q4));
+console.log('q5:', beautifulText(...q5));
+console.log('q6:', beautifulText(...q6));
+console.log('q7:', beautifulText(...q7));
+console.log('q8:', beautifulText(...q8));
+console.log('q9:', beautifulText(...q9));
+console.log('q10:', beautifulText(...q10));
+console.log('q11:', beautifulText(...q11));
